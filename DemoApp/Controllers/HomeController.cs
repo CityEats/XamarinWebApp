6cb3e743d5c5fcf26adf51a59d723b7196064941
James Ford <jimford180@gmail.com>
2015-09-21 08:14:50 -0700

    Initial.

---
+++ DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var r = new Random();
+            var restaurants = new List<RestaurantWaitTime>();
+            restaurants.Add(new RestaurantWaitTime { Name = "Fig", Minutes = r.Next(0, 60)});
+            restaurants.Add(new RestaurantWaitTime { Name = "Fork", Minutes = r.Next(0, 60) });
+            restaurants.Add(new RestaurantWaitTime { Name = "Plate", Minutes = r.Next(0, 60) });
+            restaurants.Add(new RestaurantWaitTime { Name = "Cup", Minutes = r.Next(0, 60) });
+            return View(restaurants);
+        }
+    }
+}
---
+++ DemoApp/Controllers/RestaurantWaitTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoApp.Controllers
+{
+    public class RestaurantWaitTime
+    {
+        public string Name { get; set; }
+        public int Minutes { get; set; }
+    }
+}
