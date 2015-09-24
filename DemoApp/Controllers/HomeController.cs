ad9bb586170117e1a1b31384e1757f1e875a5757
James Ford <jimford180@gmail.com>
2015-09-24 12:43:32 -0700

    Updates

--- DemoApp/Controllers/HomeController.cs
+++ DemoApp/Controllers/HomeController.cs
@@ -10,10 +10,11 @@ namespace DemoApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(bool manifold = false)
         {
             var r = new Random();
             var restaurants = new List<RestaurantWaitTime>();
+            ViewBag.Manifold = manifold;
             restaurants.Add(new RestaurantWaitTime { Name = "Fig", Minutes = r.Next(0, 5)});
             restaurants.Add(new RestaurantWaitTime { Name = "Fork", Minutes = r.Next(0, 5) });
             restaurants.Add(new RestaurantWaitTime { Name = "Plate", Minutes = r.Next(0, 5) });
